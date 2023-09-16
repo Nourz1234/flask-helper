a6e9fb96ebb35be23fd85e05686041e93546c594
Nourz1234 <104628709+Nourz1234@users.noreply.github.com>
2023-09-16 12:19:28 +0300

    remove unnecessary "AppError" exception

--- flask_helper/errors.py
+++ flask_helper/errors.py
@@ -1,13 +1,8 @@
-from werkzeug.exceptions import HTTPException, InternalServerError
+from werkzeug.exceptions import HTTPException
 from werkzeug.sansio.response import Response
 
 
-class AppError(HTTPException):
-    code = 500
-    description = InternalServerError.description
-
-
-class ValidationError(AppError):
+class ValidationError(HTTPException):
     code = 400
     description = "Validation error."
     errors: dict[str, list[str]] = None
